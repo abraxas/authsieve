15cf2745e62c438a62290f256021b5de2b603c42
C.Jay <cjay.martin@gmail.com>
2015-03-01 21:09:35 -0500

    Lots of missed files

---
+++ routes/api.js
@@ -0,0 +1,46 @@
+var express = require('express');
+var router = express.Router();
+var db = require('../lib/models');
+var _ = require('lodash');
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+router.get('/apps', function(req,res,next) {
+  db.App.findAll().then(function(apps) {
+    res.json(_.map(apps, function(app) { return app.toJSON() }));
+  });
+});
+
+router.get('/app_users', function(req,res,next) {
+  db.AppUser.findAll().then(function(appUsers) {
+    res.json(_.map(appUsers, function(appUser) { return appUser.toJSON() }));
+  });
+});
+
+router.use('/apps/:app_id', function(req,res,next) {
+  db.App.find(req.params.app_id)
+  .then(function(appRecord) {
+    req.appRecord = appRecord.toJSON();
+    next();
+  });
+});
+
+router.use('/apps/:app_id', (function() {
+  var appRouter = express.Router();
+
+  appRouter.get('/app_users', function(req,res,next) {
+    db.AppUser.findAll().then(function(appUsers) {
+      res.json(_.map(appUsers, function(appUser) { return appUser.toJSON() }));
+    });
+    
+  });
+
+
+  return appRouter;
+})())
+
+module.exports = router;
+
