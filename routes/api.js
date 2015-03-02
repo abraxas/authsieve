e4efaeb0567a3739f8c8b36ec5e1b983733ab031
C.Jay <cjay.martin@gmail.com>
2015-03-01 21:50:32 -0500

    Still going not done but whatever

--- routes/api.js
+++ routes/api.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 var db = require('../lib/models');
 var _ = require('lodash');
+var uuid = require('uuid');
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -34,8 +35,16 @@ router.use('/apps/:app_id', (function() {
   appRouter.get('/app_users', function(req,res,next) {
     db.AppUser.findAll().then(function(appUsers) {
       res.json(_.map(appUsers, function(appUser) { return appUser.toJSON() }));
+    });    
+  });
+
+  appRouter.post('/users', function(req,res,next) {
+    db.AppUser.create(req.params)
+    req.params.id = uuid();
+
+    .then(function(appUser) {
+      res.json(appUser.toJSON());
     });
-    
   });
 
 
